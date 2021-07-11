2c3d3628fb4460d5eb47e24ba1ac9554ecff67ef
rsa-rahul <54911415+rsa-rahul@users.noreply.github.com>
2021-07-12 03:45:56 +0530

    9th

---
+++ Day18/src/box1.jsx
@@ -0,0 +1,16 @@
+const styleforBox1 ={
+    backgroundColor: "black",
+    width: "12.5%",
+    height: "12.5%",
+    display: "inline-block",
+    float: "left",
+  }
+  
+  const Box1= () =>{
+    return (
+     <div style={styleforBox1}>
+     </div>
+    );
+  };
+  
+  export default Box1;
